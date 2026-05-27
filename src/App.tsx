9207737fbf5bc7bc4b6189eb644ea9755be11494
KauaGabriell <kauagabriell64@gmail.com>
2026-05-27 14:54:22 -0300

    feat: monta area principal do jogo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import styles from "./App.module.css";
 import { Header } from "./components/Header";
+import { PalpiteInput } from "./components/Input";
+import { Letter } from "./components/Letter";
 import { Tip } from "./components/Tip";
 
 export default function App() {
@@ -12,6 +14,18 @@ export default function App() {
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
         <Tip tip="Uma das linguagens de programação mais utilizadas" />
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
+
+        <h4>Palpite</h4>
+        <div>
+          <PalpiteInput autoFocus maxLength={1} placeholder="?" />
+        </div>
       </main>
     </div>
   );
