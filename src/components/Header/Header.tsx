6e0b24de6b3809dc3adc1e1c8805077ee5cf452a
KauaGabriell <kauagabriell64@gmail.com>
2026-05-27 13:43:54 -0300

    style: ajusta layout do header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import logo from "../../assets/logo.png";
+import restart from "../../assets/restart.svg";
 
 import styles from "./Header.module.css";
 
@@ -6,6 +7,20 @@ export function Header() {
   return (
     <header className={styles.container}>
       <img className={styles.logo} src={logo} alt="Advinhe" />
+
+      <div className={styles.actions}>
+        <span className={styles.attempts}>
+          <strong className={styles.attemptsCount}>5</strong> de 10 tentativas
+        </span>
+
+        <button
+          className={styles.restartButton}
+          type="button"
+          aria-label="Reiniciar jogo"
+        >
+          <img className={styles.restartIcon} src={restart} alt="" />
+        </button>
+      </div>
     </header>
   );
 }
